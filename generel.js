10d96a1a46096e3b5c23c6b9e58744d8d3b40c15
josefine <josc0003@stud.ek.dk>
2026-01-04 19:11:26 +0100

    new

---
+++ generel.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+
+window.onscroll = function () {
+  updateProgressBar();
+};
+
+function updateProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
